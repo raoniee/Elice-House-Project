d91e0600bbaa8d02fe0341aa71bb806f95f6c631
[SW5] 강성관 <seong2808@gmail.com>
2023-07-04 17:08:27 +0900

    Feat: Schema 제작 1차

--- BE/src/db/schemas/product-schema.js
+++ BE/src/db/schemas/product-schema.js
@@ -1,2 +1,35 @@
 import { Schema } from "mongoose";
-const productSchema = (new Schema);
+
+const productSchema = new Schema(
+    {
+        productId: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        prire: {
+            type: Number,
+            required: true,
+        },
+        categoryId:{
+            type: String,
+            required: true,
+        },
+        imageUrl: {
+            type: String,
+            required: true,
+        }, 
+        description: {
+            type: String,
+            required: true,
+        }
+    },{
+        collection: "products",
+        timestamp: true,
+    }
+);
+
+export { productSchema };
