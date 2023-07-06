8b65c628f8a1a0ee53821a690177764b907c46ae
[SW5] 양유진 <yoojinyang0303@gmail.com>
2023-07-06 16:39:33 +0900

    Chore: 회원가입, 로그인 분리-각 파일 중복내용 제거

--- FE/views/pages/sign-up/sign-up.js
+++ FE/views/pages/sign-up/sign-up.js
@@ -54,28 +54,3 @@ const validateSignUp = (e) => {
   //   alert(`${err.message} : 확인 후, 다시 시도해 주세요.`);
   // }
 };
-
-// 로그인 유효성 검사
-const validateLogin = (e) => {
-  // 이벤트 기본값(효과) 제거
-  e.preventDefault();
-  const INPUT_EMAIL = document.getElementById("input-email");
-  const INPUT_PW = document.getElementById("input-password");
-  const LOGIN_SUBMIT = document.getElementById("login-submit");
-
-  // 이메일 유효성 검사: 빈 칸 불가
-  if (INPUT_EMAIL.value == "") {
-    alert("이메일을 입력해주세요.");
-    INPUT_EMAIL.focus();
-    return;
-  }
-
-  // 비밀번호 유효성 검사: 빈칸 불가, 비밀번호 === 비밀번호 재확인
-  if (INPUT_PW.value == "") {
-    alert("비밀번호를 입력해주세요.");
-    INPUT_PW.focus();
-    return;
-  }
-
-  return;
-};
