20ad4a4f14a2bc08eb9c2a492d51d3d79f932dd1
[SW5] 강성관 <seong2808@gmail.com>
2023-07-06 15:09:27 +0900

    Feat: 회원가입 기능 구현

--- BE/src/controllers/user-controller.js
+++ BE/src/controllers/user-controller.js
@@ -2,13 +2,20 @@ import { UserService } from "../services/user-service";
 
 const UserController = {
     async createUser(req, res, next) {
-        const { name, email, password } = req.body;
+        try{
+            const { name, email, password } = req.body;
     
         const newUser = UserService.addUser({
             name,
             email,
             password,
         });
+
+        res.status(201).json(newUser);
+        } 
+        catch(error) {
+            throw new Error('회원가입 중 에러 발생')
+        }
     }
 }
 
--- BE/src/services/user-service.js
+++ BE/src/services/user-service.js
@@ -1,14 +1,25 @@
-import { User } from "../db/models/user-model";
+import { UserModel } from "../db/models/user-model";
+import bcrypt from 'bcrypt'
 
-const UserService = {
+class UserService {
+    async addUser(userRegist) {
+        const name = userRegist.name;
+        const email = userRegist.email;
+        const password = userRegist.password;
 
-    async addUser(userInfo) {
-        const name = userInfo.name;
-        const email = userInfo.email;
-        const password = userInfo.password;
+        // 이메일 중복 확인 
+        const user = await UserModel.findByEmail({ email }); 
+        if (user) {
+            throw new Error('이미 존재된 아이디입니다.')
+        }
 
-        
-    },
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const UserInfo = { name, email, password: hashedPassword }
+        const addNewUser  = await UserModel.create(UserInfo);
+
+        return addNewUser;
+    }
 }
 
 export { UserService }
