742b0bb79bc9607b2d3ebd4149b56e8b83fde176
RAEHEEKIM <rh.sherlock@gmail.com>
2023-07-07 20:29:56 +0900

    Feat: 장바구니 페이지 추가

---
+++ src/views/pages/order-progress/order-progress.js
@@ -0,0 +1,27 @@
+import { drawHeader } from "../../components/header/header.js";
+import { drawFooter } from "../../components/footer/footer.js";
+
+// Header, Footer 템플릿 삽입
+drawHeader();
+drawFooter();
+
+const searchAddressBtn = document.querySelector("#search-address-btn");
+searchAddressBtn.addEventListener("click", searchAddress);
+
+// 다음 주소 찾기
+function searchAddress() {
+  new daum.Postcode({
+    oncomplete: function (data) {
+      // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+      // 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
+      // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+      var roadAddr = data.roadAddress; // 도로명 주소 변수
+
+      // 우편번호와 주소 정보를 해당 필드에 넣는다.
+      document.getElementById("postcode").value = data.zonecode;
+      document.getElementById("roadAddress").value = roadAddr;
+      document.getElementById("jibunAddress").value = data.jibunAddress;
+    },
+  }).open({ autoClose: true });
+}
