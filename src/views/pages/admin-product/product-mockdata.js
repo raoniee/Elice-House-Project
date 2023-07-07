31822e4c433749f732eb05bff1927e0f14d09439
Lemon <tjdqls723@gmail.com>
2023-07-08 04:01:11 +0900

    Refactor: modal 구성요소 재구성

--- src/views/pages/admin-product/product-mockdata.js
+++ src/views/pages/admin-product/product-mockdata.js
@@ -1,17 +1,24 @@
+// 백엔드 팀원들과 상의 후 수정 예정
 export const data = [
   {
-    categoryName: "침대",
+    categoryName: "침실가구",
+    subCategoryName: "침대",
     productName: "프리미엄 가죽침대",
+    brandName: "브랜드1",
     price: "2,550,000",
+    productImage: "image1",
     createDate: "2023-07-05",
     salesVolume: "5",
     saleState: "판매중",
     detail: "좋은 가죽으로 만든 프리미엄 침대",
   },
   {
-    categoryName: "식탁",
+    categoryName: "주방가구",
+    subCategoryName: "식탁",
     productName: "한정판 원목식탁",
+    brandName: "브랜드2",
     price: "3,550,000",
+    productImage: "image2",
     createDate: "2023-07-06",
     salesVolume: "2",
     saleState: "판매중",
