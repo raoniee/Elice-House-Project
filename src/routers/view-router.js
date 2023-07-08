5d6a1341f34d6baef3340f2465c76b2240f41037
Raon <raonharu0822@kakao.com>
2023-07-08 10:44:53 +0900

    Feat: item 관련 js파일 수정

--- src/routers/view-router.js
+++ src/routers/view-router.js
@@ -1,21 +1,23 @@
-import express from "express";
-import path from "path";
+// import express from "express";
+// import path from "path";
 
-const viewsRouter = express.Router();
+// const viewsRouter = express.Router();
 
-// 페이지별로 html, css, js 파일들 라우팅
-viewsRouter.use("/", serveStatic("item-detail"));
+// // 페이지별로 html, css, js 파일들 라우팅
+// viewsRouter.use("/", serveStatic("main"));
+// viewsRouter.use("/product/list", serveStatic("item-list"));
+// viewsRouter.use("/product/detail", serveStatic("item-detail"));
 
-// views 폴더의 최상단 파일 (사진, favicon 등) 라우팅
-viewsRouter.use("/", serveStatic(""));
+// // views 폴더의 최상단 파일 (사진, favicon 등) 라우팅
+// viewsRouter.use("/", serveStatic(""));
 
-// views폴더 내의 ${resource} 폴더 내의 모든 파일을 웹에 띄우며,
-// 이 때 ${resource}.html 을 기본 파일로 설정함.
-function serveStatic(resource) {
-  const resourcePath = path.join(__dirname, `../views/pages${resource}`);
-  const option = { index: `${resource}.html` };
+// // views폴더 내의 ${resource} 폴더 내의 모든 파일을 웹에 띄우며,
+// // 이 때 ${resource}.html 을 기본 파일로 설정함.
+// function serveStatic(resource) {
+//   const resourcePath = path.join(__dirname, `../views/pages/${resource}`);
+//   const option = { index: `${resource}.html` };
 
-  return express.static(resourcePath, option);
-}
+//   return express.static(resourcePath, option);
+// }
 
-export { viewsRouter };
+// export { viewsRouter };
