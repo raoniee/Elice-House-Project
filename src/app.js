9db514df3a3777aeaa483aeb6b5647a17f24124b
[SW5] 강성관 <seong2808@gmail.com>
2023-07-08 12:10:27 +0900

    Chore: BE파일 FE파일  병합

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import bodyParser from "body-parser";
 import { userRouter } from "./routers/user-router.js";
 import { categoryRouter } from "./routers/category-router.js";
 import { productRouter } from "./routers/product-router.js";
-import { categoryRouter } from "./routers/category-router.js";
 
 // dotenv.config();
 
@@ -26,6 +25,5 @@ app.get("/", (req, res) => {
 app.use("/api", userRouter);
 app.use("/api", categoryRouter);
 app.use("/api", productRouter);
-app.use("/api", categoryRouter);
 
 export { app };
