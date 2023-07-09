8afd8341c1cded269469f7285cc306b0edb34092
[SW5] 곽수경 <sky339128@gmail.com>
2023-07-10 04:39:41 +0900

    Feat: User 주문 수정, Admin 주문 수정 기능 추가

--- src/db/models/orderitem-model.js
+++ src/db/models/orderitem-model.js
@@ -4,20 +4,26 @@ import { orderItemSchema } from "../schemas/orderitem-schema.js";
 const OrderItem = model("orderItems", orderItemSchema);
 
 class OrderitemModel {
-    async create(newOrderitem) {
-        const createOrderitem = await OrderItem.create(newOrderitem);
-        return createOrderitem;
-    }
+  async create(newOrderitem) {
+    const createOrderitem = await OrderItem.create(newOrderitem);
+    return createOrderitem;
+  }
 
-    async getOrderItem(userId) {
-        const orderItemInfo = await OrderItem.find({ userId });
-        return orderItemInfo;
-    }
+  async getOrderItem(userId) {
+    const orderItemInfo = await OrderItem.find({ userId });
+    return orderItemInfo;
+  }
 
-    async getOrderOne(orderId) {
-        const orderOneInfo = await OrderItem.findOne({ orderId });
-        return orderOneInfo;
-    }
+  async getOrderOne(orderId) {
+    const orderOneInfo = await OrderItem.findOne({ orderId });
+    return orderOneInfo;
+  }
+
+  // orderId를 가지고 orderItems 삭제
+  async deleteOrder(orderId) {
+    const deleteData = await OrderItem.deleteOne({ _id: orderId });
+    return deleteData;
+  }
 }
 
 const orderitemModel = new OrderitemModel();
--- src/services/order-service.js
+++ src/services/order-service.js
@@ -2,66 +2,92 @@ import { productModel } from "../db/models/product-model.js";
 import { orderitemModel } from "../db/models/orderitem-model.js";
 import { orderModel } from "../db/models/order-model.js";
 
-
 class OrderService {
-    async addOrder(newOrder, newOrderitem) {
-        const createOrder = await orderModel.create(newOrder);
-        let createOrderitems = [];
-        let result = [];
-        let createOrderitem= {};  
-        if (createOrder) {
-            
-            const orderId = createOrder._id;
-            const productIds = newOrderitem.productIds;
-            for (const productId of productIds) {
-                const orderProduct  = await productModel.getProdInfo(productId);
-                const productName = orderProduct.name;
-                const productImg = orderProduct.imageUrl;
+  async addOrder(newOrder, newOrderitem) {
+    const createOrder = await orderModel.create(newOrder);
+    let createOrderitems = [];
+    let result = [];
+    let createOrderitem = {};
+    if (createOrder) {
+      const orderId = createOrder._id;
+      const productIds = newOrderitem.productIds;
+      for (const productId of productIds) {
+        const orderProduct = await productModel.getProdInfo(productId);
+        const productName = orderProduct.name;
+        const productImg = orderProduct.imageUrl;
 
-                createOrderitem.orderId = orderId;
-                createOrderitem.productId = productId;
-                createOrderitem.productName = productName;
-                createOrderitem.productImg = productImg;
-                createOrderitem.quantity = 0;
+        createOrderitem.orderId = orderId;
+        createOrderitem.productId = productId;
+        createOrderitem.productName = productName;
+        createOrderitem.productImg = productImg;
+        createOrderitem.quantity = 0;
 
-                createOrderitems.push(createOrderitem);
-            }
-            
-            const quantitys = newOrderitem.quantitys;
-            let cnt = 0;
+        createOrderitems.push(createOrderitem);
+      }
 
-            for (const quantity of quantitys) {
-                createOrderitems[cnt].quantity = quantity;
-                const addOrderIfo = await orderitemModel.create(createOrderitems[cnt]);
-                if (addOrderIfo) {
-                    result.push(addOrderIfo)
-                }
-                cnt++;
-            }
+      const quantitys = newOrderitem.quantitys;
+      let cnt = 0;
 
+      for (const quantity of quantitys) {
+        createOrderitems[cnt].quantity = quantity;
+        const addOrderIfo = await orderitemModel.create(createOrderitems[cnt]);
+        if (addOrderIfo) {
+          result.push(addOrderIfo);
         }
-        const order = {
-            createOrder : createOrder,
-            createOrderitems: result
-        }
-        
-        return order;
+        cnt++;
+      }
+    }
+    const order = {
+      createOrder: createOrder,
+      createOrderitems: result,
+    };
+
+    return order;
+  }
+
+  async getOrder(userId) {
+    const orders = await orderModel.getOrder(userId);
+    let orderInfo = [];
+
+    for (const order of orders) {
+      const orderId = order._id;
+      if (orderId) {
+        const orderItem = await orderitemModel.getOrderOne(orderId);
+        orderInfo.push(orderItem);
+      }
     }
 
-    async getOrder(userId) {
-        const orders = await orderModel.getOrder(userId);
-        let orderInfo = [];
+    return orderInfo;
+  }
 
-        for (const order of orders) {
-            const orderId = order._id;
-            if (orderId) {
-                const orderItem = await orderitemModel.getOrderOne(orderId);
-                orderInfo.push(orderItem);
-            }
-        }
+  // 수신 받은 주문 정보 수정
+  async updateOrderInfo(orderId, toUpdate) {
+    const checkUpdate = await orderModel.update(orderId, toUpdate);
+    return checkUpdate;
+  }
 
-        return orderInfo;
+  async updateOrderByAdmin() {}
+
+  // 주문의 배송상태 확인
+  async getStateById(orderId) {
+    console.log(orderId);
+    const order = await orderModel.findByUserId(orderId);
+    console.log(order);
+    console.log(orderId);
+    const checkOrderState = order.state == "배송준비중";
+    return checkOrderState;
+  }
+
+  // orderId를 가지고 order 삭제
+  async deleteOrder(orderId) {
+    const deleteData = await orderModel.deleteByOrderId(orderId);
+
+    if (deleteData.deletedCount === 0) {
+      throw new Error("주문 삭제 실패");
     }
+
+    return { result: "success" };
+  }
 }
 
 const orderService = new OrderService();
