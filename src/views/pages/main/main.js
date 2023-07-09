559abff4f891ee5c49f10001fcfeb02e80292895
Raon <raonharu0822@kakao.com>
2023-07-09 22:23:35 +0900

    Feat: header 수정

--- src/views/pages/main/main.js
+++ src/views/pages/main/main.js
@@ -1,6 +1,9 @@
 import { drawHeader } from "../components/header/header.js";
+import { insertHeaderData } from "../components/header/header.js";
 import { drawFooter } from "../components/footer/footer.js";
 
 // Header, Footer 템플릿 삽입
 drawHeader();
 drawFooter();
+
+insertHeaderData();
