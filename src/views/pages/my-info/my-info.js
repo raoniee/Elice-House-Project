4ab778f02bf07a04d0a4a8d8038b2896d7d4c85b
RAEHEEKIM <rh.sherlock@gmail.com>
2023-07-10 16:28:56 +0900

    Feat: 마이페이지 기능 구현(더미데이터)

--- src/views/pages/my-info/my-info.js
+++ src/views/pages/my-info/my-info.js
@@ -1,14 +1,18 @@
-import { drawHeader } from "../../components/header/header.js";
-import { drawFooter } from "../../components/footer/footer.js";
-import { drawMyNav } from "../../components/my-nav/my-nav.js";
+import { drawHeader } from "../components/header/header.js";
+import { insertHeaderData } from "../components/header/header.js";
+import { drawFooter } from "../components/footer/footer.js";
+import { drawMyNav } from "../components/my-nav/my-nav.js";
 import * as Api from "../../api.js";
 
 // Header, Footer 템플릿 삽입
 drawHeader();
+insertHeaderData();
 drawFooter();
 
 // 마이페이지 사이드메뉴 템플릿 삽입
 drawMyNav();
+
+getUserData();
 
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
@@ -16,50 +20,55 @@ const passwordInput = document.querySelector("#password");
 const password2Input = document.querySelector("#password2");
 const saveInfoChangeBtn = document.querySelector("#save-info-change-btn");
 
-saveInfoChangeBtn.addEventListener("click", saveInfoChange);
-
-let userData;
+// let userData;
 async function getUserData() {
-  userData = await Api.get("/api/users");
+  // userData = await Api.get("/api/users/userId");
 
-  const { name, email } = userData;
-  userData.password = "";
+  fetch("dummy.json")
+    .then((response) => response.json())
+    .then((userData) => {
+      const { name, email } = userData;
+      userData.password = "";
 
-  nameInput.value = name;
-  emailInput.value = email;
-}
+      nameInput.value = name;
+      emailInput.value = email;
 
-async function saveInfoChange(e) {
-  e.preventDefault();
+      saveInfoChangeBtn.addEventListener("click", saveInfoChange);
+      function saveInfoChange(e) {
+        e.preventDefault();
 
-  const changedData = {};
-  const name = nameInput.value;
-  const password = passwordInput.value;
-  const password2 = password2Input.value;
+        const changedData = {};
+        const name = nameInput.value;
+        const password = passwordInput.value;
+        const password2 = password2Input.value;
 
-  if (name !== userData.name) {
-    if (name === "") {
-      return alert("이름을 입력해주세요.");
-    } else {
-      changedData.name = name;
-    }
-  }
+        if (name !== userData.name) {
+          if (name === "") {
+            return alert("이름을 입력해주세요.");
+          } else {
+            changedData.name = name;
+          }
+        }
 
-  if (password !== password2) {
-    return alert("비밀번호와 비밀번호 재확인이 일치하지 않습니다.");
-  } else {
-    changedData.password = password;
-  }
+        if (password !== "" || password2 !== "") {
+          if (password !== password2) {
+            return alert("비밀번호와 비밀번호 재확인이 일치하지 않습니다.");
+          } else {
+            changedData.password = password;
+          }
+        }
 
-  if (Object.keys(changedData).length === 0) {
-    return alert("수정된 정보가 없습니다");
-  }
-
-  // 수정 사항 업데이트
-  try {
-    await Api.patch("/api/users", email, changedData);
-    alert("수정 사항이 저장되었습니다.");
-  } catch (err) {
-    alert(`오류가 발생하였습니다: ${err}`);
-  }
+        if (Object.keys(changedData).length === 0) {
+          return alert("수정된 정보가 없습니다");
+        }
+        console.log(changedData);
+        // 수정 사항 업데이트
+        // try {
+        //   await Api.patch("/api/users", email, changedData);
+        //   alert("수정 사항이 저장되었습니다.");
+        // } catch (err) {
+        //   alert(`오류가 발생하였습니다: ${err}`);
+        // }
+      }
+    });
 }
