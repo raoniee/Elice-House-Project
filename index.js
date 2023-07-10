8364c6144b0fd639149ef863e2013b88a8df9288
RAEHEEKIM <rh.sherlock@gmail.com>
2023-07-10 17:35:55 +0900

    Rename: DB 연결 관련

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+import { mongoose } from "mongoose";
 import "dotenv/config";
 import { app } from "./src/app.js";
 
-const PORT = process.env.PORT || 5000;
+const { PORT, DB_URL } = process.env;
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("open", () => console.log("☑️  Connected to DB"));
+db.on("error", (error) => console.log("❌  DB Error", error));
 
 app.listen(PORT, () => {
   console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
